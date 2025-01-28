e29cce87728cb5ad5fbb355e8f93ce0b69f1e857
Saeed Bayat <01shadowalker01@gmail.com>
2025-01-28 11:16:55 +0330

    refactor: implement the board with two loops

--- week-2/src/App.tsx
+++ week-2/src/App.tsx
@@ -9,27 +9,22 @@ export default function Board() {
     setSquares(nextSquares);
   }
 
-  return (
-    <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
+  let counter = 0;
+  const template = [0, 1, 2].map((rowIndex) => {
+    return <div className="board-row" key={'row-' + rowIndex}>
+      {
+        squares.slice(rowIndex * 3, (rowIndex + 1) * 3).map((square) => {
+          const index = counter
+          counter++
+          return <Square value={square} key={counter} onSquareClick={() => handleClick(index)} />
+        })
+      }
+    </div>
+  })
+
+  return <>{template}</>;
 }
 
-function Square({value,onSquareClick  }: {value: string,onSquareClick: any  }) {
+function Square({ value, onSquareClick }: { value: string, onSquareClick: any }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
