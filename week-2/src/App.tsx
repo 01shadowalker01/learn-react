1bd74f3ae8c63804fb990eb8785dfc82e9636489
Saeed Bayat <01shadowalker01@gmail.com>
2025-02-15 16:05:52 +0330

    feat: implement time travel feature

--- week-2/src/App.tsx
+++ week-2/src/App.tsx
@@ -5,6 +5,7 @@ type Player = "X" | "O";
 export default function Board() {
 
   const [squares, setSquares] = useState<SquareValue[]>(Array(9).fill(""));
+  const [history, setHistory] = useState<SquareValue[][]>([Array(9).fill("")]);
   const [turn, setTurn] = useState<Player>("X");
   const [winner, setWinner] = useState<Player | null>(null);
 
@@ -12,10 +13,13 @@ export default function Board() {
     if (winner) return;
 
     const nextSquares = squares.slice();
+    const nextHistory = history.slice();
     const isEmpty = nextSquares[index] == "";
     if (isEmpty) {
       nextSquares[index] = turn;
       setSquares(nextSquares);
+      nextHistory.push(nextSquares)
+      setHistory([...history, nextSquares])
       switchTurn();
     }
 
@@ -48,16 +52,25 @@ export default function Board() {
 
   function checkForWinner(s1: SquareValue, s2: SquareValue, s3: SquareValue): Player | null {
     if (s1 === "X" || s1 === "O") {
-      if (s1 === s2 && s2 === s3) {
-        return s1;
-      }
+      if (s1 === s2 && s2 === s3) return s1;
     }
 
     return null;
   }
 
+  function travelToMoveNumber(moveNumber: number) {
+    const isNotLastMove = history.length - 1 !== moveNumber;
+    if (winner && isNotLastMove) setWinner(null);
+    const nextHistory = history.slice(0, moveNumber + 1);
+    setHistory(nextHistory)
+    const lastItemIndex = nextHistory.length - 1
+    setSquares(nextHistory[lastItemIndex])
+    const nextTurn: Player = moveNumber % 2 === 0 ? "X" : "O";
+    setTurn(nextTurn);
+  }
+
   let counter = 0;
-  const template = [0, 1, 2].map((rowIndex) => {
+  const boardTemplate = [0, 1, 2].map((rowIndex) => {
     return <div className="board-row" key={'row-' + rowIndex}>
       {
         squares.slice(rowIndex * 3, (rowIndex + 1) * 3).map((square) => {
@@ -69,13 +82,31 @@ export default function Board() {
     </div>
   })
 
+  const historyTemplate = history.map((item, index) => {
+    if (history.length < 2 || index == 0) return;
+
+    return <button key={index} onClick={() => travelToMoveNumber(index)}>Go to move #{index}</button>
+  })
+
   return <>
-    <p>Turn: {turn}</p>
-    {template}
-    {winner ? `${winner} is winner` : ''}
+    <p>{winner ? `${winner} is winner` : `Turn: ${turn}`}</p>
+    <div className="wrapper">
+      <div className="board">
+        {boardTemplate}
+      </div>
+      <div className="history">
+        <button onClick={() => travelToMoveNumber(0)}>Go to game start</button>
+        {historyTemplate}
+      </div>
+    </div>
   </>;
 }
 
-function Square({ value, onSquareClick }: { value: string, onSquareClick: any }) {
+type SquareInputs = {
+  value: string;
+  onSquareClick: any;
+};
+
+function Square({ value, onSquareClick }: SquareInputs) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
