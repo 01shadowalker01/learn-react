70196e54cea33f75ce44e70c3e7136b61aa17a31
Saeed Bayat <01shadowalker01@gmail.com>
2025-05-08 16:57:31 +0330

    feat: add a little emoji

--- week-3/src/App.tsx
+++ week-3/src/App.tsx
@@ -5,7 +5,7 @@ import TodoItems from "./components/TodoItems";
 export default function App() {
   return (
     <section className="flex flex-col mt-5 ms-5">
-      <h1 className="mb-3 text-2xl">Todo List</h1>
+      <h1 className="mb-3 text-2xl">Todo List 🎈</h1>
       <div className="items">
         <TodoItems />
       </div>
